f68428ee815883003ed516d706bf51497d15bd87
Felix <774284012@qq.com>
2016-04-15 18:13:53 +0800

    share add temperature

--- src/apps/share/views.py
+++ src/apps/share/views.py
@@ -20,8 +20,13 @@ class ShareList(BaseView, PaginateMixin):
 
     def get(self, request, *args, **kwargs):
         share = Share.objects.filter(is_active=True).order_by('-publish_time')
+        code = request.COOKIES.get('code')
+        now_tmp = request.COOKIES.get('now_tmp')
+        txt = request.COOKIES.get('txt')
+        pic_url = 'http://files.heweather.com/cond_icon/%s.png' % (code,)
 
         page_obj, queryset = self.paginate_queryset(share, self.page_size)
-        kwargs.update(share=queryset, page_obj=page_obj)
+        kwargs.update(share=queryset, page_obj=page_obj, txt=txt, now_tmp=now_tmp,
+                      pic_url=pic_url, flag=True if code and now_tmp and txt else False)
 
         return super(ShareList, self).get(request, **kwargs)
